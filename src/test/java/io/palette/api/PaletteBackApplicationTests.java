6ee083d327ff57f984fe22ea60fdbeaf6e9c886c
anpopo <dkstpgud@gmail.com>
2021-09-30 21:12:22 +0900

    initial commit

---
+++ src/test/java/io/palette/api/PaletteBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.palette.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaletteBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
