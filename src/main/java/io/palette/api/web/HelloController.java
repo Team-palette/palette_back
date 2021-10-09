f4de16b6c7bbd8d2a084b3ec3aad16139610543b
최은지 <ejejc@joonggonara.co.kr>
2021-10-09 17:50:00 +0900

    주석달기.

--- src/main/java/io/palette/api/web/HelloController.java
+++ src/main/java/io/palette/api/web/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+// 주석 달기.
 @RequestMapping("/api")
 @RestController
 public class HelloController {
